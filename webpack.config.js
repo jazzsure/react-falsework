e9709e3eeb3079e6e80ba1455d18ec2125c4c207
zhangshuo <zhangshuo@lvwan.com>
2017-08-23 10:59:19 +0800

    master: remove web pack console

--- webpack.config.js
+++ webpack.config.js
@@ -69,7 +69,8 @@ module.exports = {
   ],
 
   devServer: {
-    contentBase: "./",//本地服务器所加载的页面所在的目录
+    contentBase: "./public",//本地服务器所加载的页面所在的目录
+    stats: 'errors-only',
     colors: true,//终端中输出结果为彩色
     historyApiFallback: true,//不跳转
     inline: true,//实时刷新
