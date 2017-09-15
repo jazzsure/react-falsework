96625014a048ef8f2082c68686f21df7b3ff0c8a
zhangshuo <zhangshuo@lvwan.com>
2017-09-15 16:23:12 +0800

    use common css

--- src/container/app/app.js
+++ src/container/app/app.js
@@ -1,6 +1,5 @@
 import React , { Component } from 'react'
-import styles from './app.scss'
-
+import commonCss from "./common.css";
 class App extends Component{
     constructor(props) {
         super(props);
